ddaf41ed5f7568c89735d83a382e20908e15dff6
Xraxus <xraxus@gmail.com>
2022-05-24 21:54:08 +0200

    Doctrine, Paginator, Encja kategorii

---
+++ app/src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Task fixtures.
+ */
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Persistence\ObjectManager;
+use Monolog\DateTimeImmutable;
+
+/**
+ * Class TaskFixtures.
+ */
+class TaskFixtures extends AbstractBaseFixtures
+{
+    /**
+     * Load data.
+     *
+     * @param ObjectManager $manager Persistence object manager
+     */
+    public function loadData(): void
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $task = new Task();
+            $task->setTitle($this->faker->sentence);
+            $task->setCreatedAt(
+                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $task->setUpdatedAt(
+                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $this->manager->persist($task);
+        }
+
+        $this->manager->flush();
+    }
+}
